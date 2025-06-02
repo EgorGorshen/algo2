d7741b5546d10aeef6d413f84bf7ea5137d7e310
EgorGorshen <egorgorshen@yandex.ru>
2025-06-02 07:55:35 +0300

    chore

---
+++ 2025-05-28/a.cpp
@@ -0,0 +1,4 @@
+l : = label.new(x = bar_index - int(sessBars / 2),
+                y = hi + syminfo.mintick * 20, text = name,
+                style = label.style_label_down, textcolor = color.white,
+                color = directionColor)
